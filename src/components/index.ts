d3b31ac97bcd4330a83efe04f4a991edf3f50bac
Percy Kuang <percykuang@foxmail.com>
2025-02-23 06:03:09 +0800

    remove all

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TopNav } from './TopNav';
-export { default as TagIcon } from './TagIcon';
-export { default as List } from './List';
--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './Home';
-export { default as Detail } from './Detail';
-export { default as Tags } from './Tags';
-export { default as TagDetail } from './TagDetail';
-export { default as AboutMe } from './AboutMe';
