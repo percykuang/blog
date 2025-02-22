186e7c30e26952768775b205c1e74b06bf77c6b3
Percy Kuang <percykuang@foxmail.com>
2025-02-23 06:39:16 +0800

    update

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,18 +1,20 @@
 import type { FC } from 'react';
 
+import { Link } from 'react-router-dom';
+
 const TopNav: FC = () => {
   return (
     <div className="flex flex-row-reverse max-md:mx-4 max-md:my-0 max-md:flex-row">
       <div className="flex text-slate-600">
-        <a href="/" className="ml-5 hover:text-emerald-300 active:text-emerald-300 max-md:m-0 max-md:mr-5">
-          文章
-        </a>
-        <a href="/tags" className="ml-5 hover:text-emerald-300 active:text-emerald-300 max-md:m-0 max-md:mr-5">
-          标签
-        </a>
-        <a href="/about-me" className="ml-5 hover:text-emerald-300 active:text-emerald-300 max-md:m-0 max-md:mr-5">
-          关于我
-        </a>
+        <Link to="/">
+          <span className="ml-5 hover:text-emerald-300 active:text-emerald-300 max-md:m-0 max-md:mr-5">文章</span>
+        </Link>
+        <Link to="/tags">
+          <span className="ml-5 hover:text-emerald-300 active:text-emerald-300 max-md:m-0 max-md:mr-5">标签</span>
+        </Link>
+        <Link to="/about-me">
+          <span className="ml-5 hover:text-emerald-300 active:text-emerald-300 max-md:m-0 max-md:mr-5">关于我</span>
+        </Link>
       </div>
     </div>
   );
