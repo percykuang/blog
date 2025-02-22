c7b81ed3813bc248e649686ea6aea9713cf9a594
Percy Kuang <percykuang@foxmail.com>
2025-02-23 07:16:22 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import type { FC } from 'react';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import { TopNav } from '@/components';
 import { AboutMe, Detail, Home, TagDetail, Tags } from '@/pages';
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="max-w-192 mx-auto mb-0 mt-10 pb-20">
         <TopNav />
         <div className="max-md:mx-4 max-md:my-0">
@@ -20,7 +20,7 @@ const App: FC = () => {
           </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
